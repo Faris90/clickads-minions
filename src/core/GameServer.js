6dba9ff0cef73d6f901c3a602942eab7f9d7c57c
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-12-01 20:57:19 -0600

    Update GameServer.js

--- src/core/GameServer.js
+++ src/core/GameServer.js
@@ -20,7 +20,12 @@ const Logger = require('../modules/log');
 const StatServer = require('./StatServer.js');
 const GeneratorService = require('./GeneratorService.js');
 const PluginLoader = require('./PluginLoader.js');
-
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
 
 module.exports = class GameServer {
   constructor(world, consoleService, configService, version, port, ismaster, name, banned, multiverse, gamemode) {
